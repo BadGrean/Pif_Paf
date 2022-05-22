6b3ef0b530f08138b683ecd7d233454f08870820
OSABOSA <0oski0@gmail.com>
2022-05-23 01:54:56 +0200

    some more changes

--- Pif_Paf/Pif_Paf/src/Game.cpp
+++ Pif_Paf/Pif_Paf/src/Game.cpp
@@ -1,37 +1,57 @@
 #include "Game.h"
 #include <iostream>
 
-SDL_Texture* playerTex;
-SDL_Rect srcR, dstR;
+SDL_Texture* playerTexture, *targetTexture, *pauseTexture;
+SDL_Rect srcPlayerRect, dstPlayerRect, dstTargetRect;
 
 Game::Game()
 {
 
 }
+
+
 Game::~Game()
 {
 
 }
+
+
+void Game::dataInit()
+{
+
+	dstPlayerRect.x = 0;
+	dstPlayerRect.y = 0;
+	dstPlayerRect.h = 32;
+	dstPlayerRect.w = 32;
+
+	dstTargetRect.x = 100;
+	dstTargetRect.y = 100;
+	dstTargetRect.h = 64;
+	dstTargetRect.w = 64;
+
+}
+
 
 void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
 	int flags = 0;
 	if (fullscreen)
 	{
-		flags = SDL_WINDOW_FULLSCREEN;
+		flags = SDL_WINDOW_FULLSCREEN_DESKTOP; //added DESKTOP - Alt+Tab does not crash
 	}
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
 	{
 		std::cout << "Subsystems initialised\n";
 
-		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags); //creates window
 		if (window)
 		{
+			SDL_GetWindowSize(window, &windowSizeX, &windowSizeY);
 			std::cout << "Window created\n";
 		}
 
-		renderer = SDL_CreateRenderer(window, -1, 0);
+		renderer = SDL_CreateRenderer(window, -1, 0); //creates renderer (there may be more than one)
 		if (renderer)
 		{
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
@@ -44,9 +64,17 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		isRunning = false;
 	}
 	SDL_Surface* tmpSurface = IMG_Load("assets/Player.png");
-	playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	playerTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface); //adds image to renderer
+	SDL_FreeSurface(tmpSurface);
+	tmpSurface = IMG_Load("assets/Target.svg.png");
+	targetTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_FreeSurface(tmpSurface);
+	tmpSurface = IMG_Load("assets/pause-icon.png");
+	pauseTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
 	SDL_FreeSurface(tmpSurface);
 }
+
+
 void Game::handleEvents()
 {
 	SDL_Event event;
@@ -56,24 +84,54 @@ void Game::handleEvents()
 	case SDL_QUIT:
 		isRunning = false;
 		break;
+
+	case SDL_MOUSEBUTTONDOWN: //sets Player position to mouseclick cords
+		posX = event.button.x;
+		posY = event.button.y;
+		speedY = 0;
+		break;
+
+	case SDL_KEYDOWN:
+		if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) //after pressing Esc button
+		{
+			isPaused = !isPaused;  //for later use 
+			if (paused())
+			{
+				SDL_RenderCopy(renderer, pauseTexture, NULL, &dstPlayerRect);
+				SDL_RenderPresent(renderer);
+				SDL_Delay(3000);//instead of menu
+				setLastFrameTime();
+			}
+			dstTargetRect.x = rand() % windowSizeX + 1; //places Target randomly 
+			dstTargetRect.y = rand() % windowSizeY + 1;
+		}
+		break;
+
 	default:
 		break;
 	}
 }
+
+
 void Game::update()
 {
-	count++;
-	dstR.h = 32;
-	dstR.w = 32;
-	dstR.x = count;
+	posX++;
+	dstPlayerRect.x = int(posX);
+	speedY = speedY + acceleration * (time() - lastFrameTime()) / 1000; //grawitacja
+	posY = posY + speedY * (time() - lastFrameTime()) / 1000;
+	dstPlayerRect.y = int(posY);
+
 }
+
 
 void Game::render()
 {
 	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, playerTex, NULL, &dstR);
-	SDL_RenderPresent(renderer);
+	SDL_RenderCopy(renderer, playerTexture, NULL, &dstPlayerRect); //pre-render image at given pos
+	SDL_RenderCopy(renderer, targetTexture, NULL, &dstTargetRect);
+	SDL_RenderPresent(renderer); //pushes render to window
 }
+
 
 void Game::clean()
 {
--- Pif_Paf/Pif_Paf/src/main.cpp
+++ Pif_Paf/Pif_Paf/src/main.cpp
@@ -4,17 +4,25 @@
 #include "Game.h"
 
 Game* game = nullptr;
+Player* player = nullptr;
 
 int main(int argc, char *argv[])
 {
+	game->dataInit();
 	game = new Game();
-	game->init("Pif Paf", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+	game->init("Pif Paf", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, true);
 	while (game->running())
 	{
 		game->handleEvents();
-		game->update();
+		game->time();
+		if (game->time() > game->lastFrameTime() + 10) //100fps cap
+		{
+			game->update();
+			game->setLastFrameTime();
+		}
 		game->render();
 	}
 	game->clean();
 	return 0;
 }
+
